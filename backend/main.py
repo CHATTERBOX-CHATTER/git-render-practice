6651873eed980c615ff29319f168beb8d42a499c
Your Name <you@example.com>
2025-10-27 17:27:52 +0900

    수정함5

--- backend/main.py
+++ backend/main.py
@@ -32,7 +32,7 @@ app = FastAPI()
 # CORS 설정
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[REACT_HOST],
+    allow_origins=[ORIGIN],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
